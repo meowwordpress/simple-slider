5745943976287b0787239b31783a4233e6bae2be
Bobby Bryant <bobby.bryant@10up.com>
2016-03-21 10:15:50 -0400

    build an image slider creation function

--- Utils.js
+++ Utils.js
@@ -1,3 +1,9 @@
+/**
+ * Function creates an image element from the input src string. It also
+ * wraps the image in a div tag with class="slide"
+ * @param  {string} src The source attribute for the requested image.
+ * @return {Element}     A DOM Element
+ */
 export function createImage(src) {
   let wrapperDiv = document.createElement('div');
   wrapperDiv.setAttribute('Class', 'slide');
